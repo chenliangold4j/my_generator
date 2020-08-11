16c696cfaddee027761e77b7379cf098c43f5cb1
chenliang <chenliangxingji1@163.com>
2020-08-11 10:14:38 +0800

    init

---
+++ src/test/java/com/phantom5702/my_generator/MyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom5702.my_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
